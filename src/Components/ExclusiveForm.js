40872b82390b435fb6d7d231c215cfb38e6bc6ab
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-02-25 21:05:05 -0500

    added functionality templates

---
+++ src/Components/ExclusiveForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//add a location function(throws an alert) to request access to location based members exclusivity
+
+
+const ExclusiveForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExclusiveForm
